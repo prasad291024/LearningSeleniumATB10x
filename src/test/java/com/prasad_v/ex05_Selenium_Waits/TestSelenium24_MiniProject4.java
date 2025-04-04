6b8b72853faaf4bcce740a6a5c22f438db3f137d
prasad291024 <prasadvaliv1992@gmail.com>
2025-04-04 16:07:49 +0530

    3 April

---
+++ src/test/java/com/prasad_v/ex05_Selenium_Waits/TestSelenium24_MiniProject4.java
@@ -0,0 +1,51 @@
+package com.prasad_v.ex05_Selenium_Waits;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class TestSelenium24_MiniProject4 {
+    @Description("Verify  makemytrip")
+    @Test
+    public void testCloseModal() {
+
+        EdgeOptions edgeOptions = new EdgeOptions();
+        edgeOptions.addArguments("--incognito");
+        edgeOptions.addArguments("--start-maximized");
+
+        WebDriver driver = new EdgeDriver(edgeOptions);
+        driver.navigate().to("https://www.makemytrip.com/");
+        System.out.println(driver.getTitle());
+
+        // Condition - Explicit wait - your code
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@data-cy=\"closeModal\"]")));
+
+        WebElement closeModel = driver.findElement(By.xpath("//span[@data-cy=\"closeModal\"]"));
+        closeModel.click();
+
+
+        try {
+            Thread.sleep(15000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.quit();
+
+
+
+
+
+
+
+    }
+}
